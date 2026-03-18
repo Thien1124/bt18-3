95b72bf878a38f99a24dac380afb91e97aa41944
Nguyễn Phước Thiện <npthien124@gmail.com>
2026-03-18 15:18:12 +0700

    [done]

---
+++ controllers/users.js
@@ -0,0 +1,47 @@
+let userModel = require('../schemas/users')
+let bcrypt = require('bcrypt')
+module.exports = {
+    CreateAnUser: async function (username, password, email, role,
+        fullName, avatarUrl, status, loginCount) {
+        let newItem = new userModel({
+            username: username,
+            password: password,
+            email: email,
+            fullName: fullName,
+            avatarUrl: avatarUrl,
+            status: status,
+            role: role,
+            loginCount: loginCount
+        });
+        await newItem.save();
+        return newItem;
+    },
+    GetAnUserByUsername: async function (username) {
+        return await userModel.findOne({
+            isDeleted: false,
+            username: username
+        })
+    }, GetAnUserById: async function (id) {
+        return await userModel.findOne({
+            isDeleted: false,
+            _id: id
+        })
+    },
+    ChangePassword: async function (userId, oldPassword, newPassword) {
+        let user = await userModel.findOne({
+            _id: userId,
+            isDeleted: false
+        })
+        if (!user) {
+            throw new Error('nguoi dung khong ton tai')
+        }
+        if (!bcrypt.compareSync(oldPassword, user.password)) {
+            throw new Error('mat khau cu khong dung')
+        }
+        user.password = newPassword
+        await user.save()
+        return {
+            message: 'doi mat khau thanh cong'
+        }
+    }
+}
